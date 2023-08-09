f291ae009a970e9a6273ad582c72220bdc1b7804
maksdebowski <maks.debowski@gmail.com>
2023-08-09 10:56:44 +0200

    added controller?///

--- src/main/java/com/baeldung/h2/h2course/controller/BookController.java
+++ src/main/java/com/baeldung/h2/h2course/controller/BookController.java
@@ -3,12 +3,9 @@ package com.baeldung.h2.h2course.controller;
 
 import com.baeldung.h2.h2course.models.Book;
 import com.baeldung.h2.h2course.services.BookService;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/books")
